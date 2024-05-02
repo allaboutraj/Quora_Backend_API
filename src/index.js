c044dccf0d082841c8587e249caf15dd6276a212
allaboutraj <rajgupta9507321@gmail.com>
2024-05-03 01:27:09 +0530

    Feature : Added routing structure for versioned API user

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const { PORT } = require("./config/server.config");
+const apiRouter = require("./routes");
 
 const app = express();
 
 app.use(express.json());
 app.use(express.text());
 app.use(express.urlencoded({ extended: true }));
+
+app.use("/api", apiRouter);
 
 app.get("/ping", (req, res) => {
   res.json({ message: "Ping Success" });
